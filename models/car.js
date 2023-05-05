7ad311ed0f67d63f5f87f5d5f3865255e789e8ff
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-05-06 01:12:42 +0700

    db car

---
+++ models/car.js
@@ -0,0 +1,32 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const Cars = db.define('users',{
+    name_mobil: {
+        type: DataTypes.STRING
+    },
+    harga_mobil: {
+        type: DataTypes.INTEGER
+    },
+    manufacture: {
+        type: DataTypes.STRING
+    },
+    year_mobil: {
+        type: DataTypes.INTEGER
+    },
+    available: {
+        type: DataTypes.BOOLEAN
+    },
+    transmission: {
+        type: DataTypes.STRING
+    },
+    refresh_token: {
+        type: DataTypes.TEXT
+    }
+},{
+    freezeTableName: true
+});
+
+export default Cars;
