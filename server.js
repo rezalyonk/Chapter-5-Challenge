89efcf55a4abd2d09afd65895687abfc71487c2e
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-05-06 01:42:24 +0700

    perubahan code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/Database.js";
+import router from "./routes/index.js";
 
 const app = express();
 const host = 'localhost';
@@ -11,6 +12,12 @@ try {
 } catch (error) {
     console.error(error);
 }
+
+
+// agar bisa menerima data dalam format json
+app.use(express.json());
+// menuju apis
+app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Server sudah berjalan cuy, silahkan buka http://localhost:${PORT}`);
