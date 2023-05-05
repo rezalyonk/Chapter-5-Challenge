622c5255647171402256fd8dd5c37d6b665c0f68
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-05-06 00:46:55 +0700

    code db and console log db

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from "express";
+import db from "./config/Database.js";
 
 const app = express();
-
 const host = 'localhost';
 const PORT = 8000;
+
+try {
+    await db.authenticate();
+    console.log('Database telah koneksi secara local loh');
+} catch (error) {
+    console.error(error);
+}
 
 app.listen(PORT, () => {
     console.log(`Server sudah berjalan cuy, silahkan buka http://localhost:${PORT}`);
