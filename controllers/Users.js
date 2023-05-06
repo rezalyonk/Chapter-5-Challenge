47a9cbb6496038e0cc5b65a0bf2e35a9bd60248c
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-05-06 23:07:56 +0700

    code register

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,4 +1,5 @@
 import Users from "../models/user.js";
+import bcrypt from "bcrypt";
 
 export const getUsers = async(req, res) => {
     try {
@@ -6,6 +7,23 @@ export const getUsers = async(req, res) => {
             attributes: ['id','name','email']
         });
         res.json(users);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const Register = async (req, res) => {
+    const { name, email, password, confPassword } = req.body;
+    if(password !== confPassword) return res.status(400).json({msg: "password dan confirm password tidak cocok"});
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+    try {
+        await Users.create({
+            name: name,
+            email: email,
+            password: hashPassword
+        });
+        res.json({msg: "register berhasil"});
     } catch (error) {
         console.log(error);
     }
